9d196c7421569e26e36febee6b0675ee50afa517
AnupJoseph <43500348+AnupJoseph@users.noreply.github.com>
2025-07-20 14:25:46 +0000

    Finished reading options

--- wordcount/main.cpp
+++ wordcount/main.cpp
@@ -1,3 +1,31 @@
-#include<iostream>
-#include<getopt.h>
+#include <getopt.h>
+#include <iostream>
 
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  int opt;
+  while ((opt = getopt(argc, argv, "clwm")) != -1) {
+    switch (opt) {
+    case 'l':
+      std::cout << "Option l selected\n";
+      break;
+    case 'c':
+      std::cout << "Option c selected with value";
+      break;
+    case 'w':
+      std::cout << "Option w selected\n";
+      break;
+    case 'm':
+      std::cout << "Option m selected\n";
+      break;
+    case '?':
+      printf("unknown option : %c\n", optopt);
+      break;
+    }
+  }
+  for (; optind < argc; optind++) {
+    printf("extra arguments: %s\n", argv[optind]);
+  }
+  return 0;
+}
